7540051fbbfd78120f6f0bf0d0f23e6d098c8475
Claudy <120015915+ClaudeMKA@users.noreply.github.com>
2024-01-29 16:03:02 +0100

    crud

--- app/Controllers/Http/BlogsController.ts
+++ app/Controllers/Http/BlogsController.ts
@@ -1,42 +1,70 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Post from "App/Models/Post";
 import UptadePostValidator from "App/Validators/UptadePostValidator";
+import Database from "@ioc:Adonis/Lucid/Database";
+import Category from "App/Models/Category";
 
 export default class BlogsController {
 
-  async index({view}: HttpContextContract) {
-    const posts = await Post.all()
+  async index({view, request}: HttpContextContract) {
+    let page = request.input('page',1)
+    const posts = await Database.from(Post.table).paginate(page, 2)
 
     return view.render('blog/index', {
       posts, // Corrected the variable name to "posts"
     })
   }
 
+  async create({ view }: HttpContextContract) {
+    const post = new Post();
+    const categories = await Category.all()
+    console.log(categories)
+    return view.render('blog/create',{
+      categories,
+      post
+    }) // Assurez-vous que ce template existe
+  }
+
   async show({params, view}: HttpContextContract) {
-    const post = await Post.findOrFail(params.id)
+
+    const post = await Post.query().preload('category').where('id',params.id).firstOrFail()
+    const categories = await Category.all()
+
     return view.render('blog/show', {
       post,
+      categories
     })
   }
 
+async store({params, request, session, response }: HttpContextContract){
+  await this.handleRequest(params,request)
+  session.flash({success: "L\'article a bien été crée"})
+  return response.redirect().toRoute('home')
+
+  }
+
+
+
+
   async update({ params, request, response, session }: HttpContextContract) {
-    try {
-      const post = await Post.findOrFail(params.id);
-      await request.validate(UptadePostValidator); // Valider les données
+   await this.handleRequest(params,request)
+    session.flash({success: "L\'articcle a bien été sauvegardé"})
+    return response.redirect().toRoute('home')
+  }
 
-      post.merge(request.all()).save();
+  async destroy({params,session, response}: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+     await post.delete()
+    session.flash({success: "L\'article a bien été supprimer"})
+    return response.redirect().toRoute('home')
 
-      session.flash({ success: "L'article a bien été sauvegardé" });
-
-      return response.redirect().toRoute('home');
-    } catch (error) {
-      // Afficher les erreurs dans la console
-      console.log(error.messages);
-
-      // Gérer les erreurs de validation ici
-      session.flash({ error: "Erreur de validation" }); // Remplacez par un message plus précis
-      return response.redirect().back();
-    }
+  }
+  private async handleRequest(params: HttpContextContract['params'], request: HttpContextContract['request']) {
+    const post = params.id ? await Post.findOrFail(params.id) : new Post();
+    const data = await request.validate(UptadePostValidator)
+    post
+      .merge({...data, online: data.online || false})
+      .save()
   }
 
 
--- start/routes.ts
+++ start/routes.ts
@@ -21,7 +21,14 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 
+Route.group(() => {
+  Route.get('/article/new', 'BlogsController.create').as('posts.create') // Afficher le formulaire de création
+  Route.post('/article/new', 'BlogsController.store') // Gérer la création
+  Route.get('/article/:id', 'BlogsController.show').as('posts.show')
+  Route.post('/article/:id', 'BlogsController.update').as('posts.update')
+  Route.delete('/article/:id', 'BlogsController.destroy') // Gérer la création
+})
+Route.get('/', 'BlogsController.index').as('home')
 
-Route.get('/','BlogsController.index').as('home')
-Route.get('/article/:id','BlogsController.show').as('posts.show')
-Route.post('/article/:id','BlogsController.update')
+
+
